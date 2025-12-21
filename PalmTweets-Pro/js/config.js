e4439a797ede98c25e6be54c3023105128e93d3d
palmtweets <useinstafy@gmail.com>
2025-12-21 19:48:42 +0300

    Delete PalmTweets-Pro directory

--- PalmTweets-Pro/js/config.js
+++
@@ -1,9 +0,0 @@
-export const SUPABASE_URL = 'https://nizxfcmrruzdduseudwj.supabase.co';
-export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5penhmY21ycnV6ZGR1c2V1ZHdqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYyOTU4ODUsImV4cCI6MjA4MTg3MTg4NX0.Ys5aVrCnNC3ur-24HixBDMlybbY5dNkCHFj5dYMiJ5I';
-
-export const UNIVERSITIES = [
-    "University of Dar es Salaam (UDSM)", 
-    "University of Dodoma (UDOM)", 
-    "IFM", "Ardhi University", "Mzumbe", 
-    "SUA", "DIT", "St. Joseph", "Mwl.Nyerere University"
-];
