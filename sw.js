ce994fc85495b42ab716838588c24d919ec6175b
palmtweets <useinstafy@gmail.com>
2025-12-22 12:48:45 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,25 @@
-const CACHE_NAME = 'palmtweets-pro-v1';
+const CACHE_NAME = 'palmtweets-v1';
 const ASSETS = [
   './',
   './index.html',
   './css/style.css',
-  './js/app.js',
+  './js/config.js',
+  './js/utils.js',
   './js/auth.js',
   './js/feed.js',
   './js/admin.js',
-  './js/utils.js',
-  './js/config.js',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  './icons/apple-icon.png'
+  './js/app.js',
+  './manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/@phosphor-icons/web'
 ];
 
-// Install Event - Cache Files
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
-// Fetch Event - Serve from Cache if available
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => response || fetch(e.request))
