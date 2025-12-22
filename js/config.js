ce51ea0f0994c9e03f87ca7adb00fd5953fab27d
palmtweets <useinstafy@gmail.com>
2025-12-22 12:43:21 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@ const SUPABASE_URL = 'https://kszlwvapzsqwxylpeelt.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtzemx3dmFwenNxd3h5bHBlZWx0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYzODA0MDUsImV4cCI6MjA4MTk1NjQwNX0.qx6zB3KUcg1ZbJOJ6eoPh4XWDrvpaNFdsA-WT72niPs';
 
 // Initialize Supabase Client
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = supabase.createClient(https://kszlwvapzsqwxylpeelt.supabase.co, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtzemx3dmFwenNxd3h5bHBlZWx0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYzODA0MDUsImV4cCI6MjA4MTk1NjQwNX0.qx6zB3KUcg1ZbJOJ6eoPh4XWDrvpaNFdsA-WT72niPs);
 
 // Global Constants
 const universities = [
@@ -21,3 +21,4 @@ const universities = [
 
 // Global State
 let currentUser = null;
+
