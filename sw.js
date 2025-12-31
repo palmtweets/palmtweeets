9fe405e1336434d2a796fda5123c823206474ad8
palmtweets <useinstafy@gmail.com>
2026-01-01 01:58:17 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // BADILISHA HII KILA UKIONGEZA FEATURES MPYA
-const CACHE_NAME = 'v-final-video-KEYSTONE'; 
+const CACHE_NAME = 'v-final-video-KEYS'; 
 
 const ASSETS = [
   './',
@@ -53,6 +53,7 @@ self.addEventListener('fetch', (e) => {
     })
   );
 });
+
 
 
 
